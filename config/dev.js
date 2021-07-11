c6683934cbdeaad79fa54a841b96a9ffbdeae56b
Balaji Kotni <67455293+Balaji-Kotni@users.noreply.github.com>
2021-07-11 18:28:12 +0530

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "DEVELOPMENT SERVER",
   PORT: 8082,
   MONGOURI:
-    "mongodb+srv://balaji:balaji1656@texteditor.gbveq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    "",
   BASE_URL: "http://localhost:3000",
   JWT_SECRET: "balajitexteditor",
   JWT_EXPIRES_IN: "9999",
